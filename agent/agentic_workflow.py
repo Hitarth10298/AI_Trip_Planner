f35f0773ccba581dcc3e2b2bcf0b62551dca78bc
Hitarth10298 <hitarthshah10298@gmail.com>
2025-09-10 11:36:43 -0700

    code updated for loading the model

---
+++ agent/agentic_workflow.py
@@ -0,0 +1,16 @@
+from utils.model_loader import ModelLoader
+from prompt_library.prompt import SystemPrompt
+from langgraph.graph import StateGraph, MessagesState, END, START
+from langgraph.prebuilt import ToolNode, tools_condition
+
+class GraphBuilder():
+    def __init__(self):
+        pass
+    
+    def agent_function(self):
+        pass
+
+    def build_graph(self):
+        pass
+
+    
