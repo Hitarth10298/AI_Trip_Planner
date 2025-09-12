099a3400fd9b0a6d23c88c2e27fe45a63bdc00f8
Hitarth10298 <hitarthshah10298@gmail.com>
2025-09-12 12:12:58 -0700

    tool flow updated

---
+++ tools/weather_info_tool.py
@@ -0,0 +1,37 @@
+import os
+from utils.weather_info import WeatherForecastTool
+from langchain.tools import tool
+from typing import List
+from dotenv import load_dotenv
+
+class WeatherInfoTool:
+    def __init__(self):
+        load_dotenv()
+        self.api_key = os.getenv("OPENWEATHER_API_KEY")
+        self.weather_service = WeatherForecastTool(self.api_key)
+        self.weather_tool_list = self._setup_tools()
+
+    def _setup_tools(self) -> List:
+        @tool
+        def get_current_weather(city: str):
+            weather_data = self.weather_service.get_current_weather(city)
+            if weather_data:
+                temp = weather_data.get('main', {}).get('temp', 'N/A')
+                desc = weather_data.get('weather', [{}])[0].get('description', 'N/A')
+                return f"Current weather in {city}: {temp}°C, {desc}"
+            return f"Could not retrieve weather data for {city}."
+        
+        @tool
+        def get_weather_forecast(city: str) -> str:
+            forecast_data = self.weather_service.get_weather_forecast(city)
+            if forecast_data and 'list' in forecast_data:
+                forecast_summary = []
+                for i in range(len(forecast_data['list'])):
+                    item = forecast_data['list'][i]
+                    date = item['dt_txt'].split(' ')[0]
+                    temp = item['main']['temp']
+                    desc = item['weather'][0]['description']
+                    forecast_summary.append(f"{date}: {temp} degree celcius, {desc}")
+                return f"Weather forecast for {city}:\n" + "\n".join(forecast_summary)
+            return f"Could not retrieve forecast data for {city}."
+        return [get_current_weather, get_weather_forecast]
